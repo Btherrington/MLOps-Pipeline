7cc6d1f248d0a0aea9f99ee64b21ab18bb3f0155
Btherrington <btherr08@gmail.com>
2025-11-12 12:08:38 -0600

    Refactorization of ingest.py

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -8,6 +8,9 @@ from utils import configure_logger
 import sys
 
 LOG_FILE_NAME = "ingest.log"
+FAILURE_RATIO_ALLOWED = 0.1
+TIME_SLEEP_NUMBER = 1.5
+RAW_DIRECTORY_PATH = os.path.join("data", "raw")
 
 logger = logging.getLogger(__name__)
 
@@ -55,36 +58,47 @@ def get_match_data(region, match_id):
         return None
 
 def main():
-    os.makedirs("data/raw",exist_ok=True)
+    os.makedirs(RAW_DIRECTORY_PATH ,exist_ok=True)
     logger.info("Attempting to retrieve puuid")
     player_puuid = get_puuid(GAME_NAME,TAGLINE)
     if not player_puuid:
-        logger.error(f"Unsuccessful pull of player id: {player_puuid}")
+        logger.error(f"Unsuccessful pull of player id, GAME_NAME: {GAME_NAME}, TAGLINE: {TAGLINE}")
         return 1 
     logger.info(f"Successful player id pull")
+
     match_ids = get_match_ids(player_puuid, REGION)
-    if not match_ids:
-        logger.error(f"unsuccessful pull of match id: {match_ids}")
+    if match_ids is None:
+        logger.error(f"Unable to pull match id from API, inputs; player_puuid: {player_puuid} and REGION: {REGION}, script is stopping.")
         return 1
-    file_write_failures = 0
+
+
+    total_failures = 0
     number_of_match_ids = (len(match_ids))
     logger.info(f"There is {number_of_match_ids} match ids being pulled")
+    if number_of_match_ids == 0:
+        logger.warning(f"API data anomaly detected, API returned 0 matches. No new data to process")
+        return 0
+
     for match_id in match_ids:
+        time.sleep(TIME_SLEEP_NUMBER)
         single_match_data = get_match_data(REGION, match_id)
-        time.sleep(1.5)
+        filename = (f"{match_id}.json")
+        file_path = os.path.join(RAW_DIRECTORY_PATH, filename)
         if not single_match_data:
-            logger.warning(f"Data point was missing, script continuing")
+            logger.warning(f"Data point was missing {match_id}, script continuing")
+            total_failures += 1
             continue
-        file_path = f"data/raw/{match_id}.json"
+
         try:
             with open(file_path,"w") as f:
                 json.dump(single_match_data,f, indent=4)
         except (IOError, PermissionError) as e:
             logger.exception(f"Failed to write json match file: {e}")
-            file_write_failures += 1
+            total_failures += 1
             continue
-    if file_write_failures/number_of_match_ids >= 0.1:
-        logger.critical(f"More than 10% of matches were not pulled")
+    actual_failure_ratio = total_failures/number_of_match_ids
+    if actual_failure_ratio >= FAILURE_RATIO_ALLOWED:
+        logger.critical(f"Failure threshold exceeded: {FAILURE_RATIO_ALLOWED: .2%} | Actual error percent = {actual_failure_ratio: .2%} | Raw count (total failed and total number of matches): ({total_failures}/{number_of_match_ids})")
         return 1
     else:
         return 0
